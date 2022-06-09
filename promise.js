50167dc047c8f804b258fd3795b0f06671ad343b
PanduAldi <pratamapandu652@gmail.com>
2022-06-09 21:51:16 +0700

    Done: Pertemuan 10 - challenge 7 - commit pertama

--- promise.js
+++ promise.js
@@ -1,7 +1,29 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+
+  const iix = await promiseTheaterIXX();
+  const vgc = await promiseTheaterVGC();
+ 
+  return new Promise(
+    (resolve) => {
+        let hasil;
+
+        if(emosi === "marah") {  
+          const iixMarah = iix.filter((el) => el.hasil === "marah").length;
+          const vgcMarah = vgc.filter((el) => el.hasil === "marah").length;
+          hasil = iixMarah + vgcMarah;
+        } else if(emosi === "tidak marah") {
+          const iixtidakMarah = iix.filter((el) => el.hasil === "tidak marah").length;
+          const vgctidakMarah = vgc.filter((el) => el.hasil === "tidak marah").length;
+          hasil = iixtidakMarah + vgctidakMarah;
+        }
+
+        return resolve(hasil);
+    }
+  )
+};
 
 module.exports = {
   promiseOutput,
